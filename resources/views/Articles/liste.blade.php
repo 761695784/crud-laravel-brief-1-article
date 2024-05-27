173b4fc2e497ebaf1cd73e89d6e2e2bb85afa559
761695784 <malcom70976@gmail.com>
2024-05-27 11:07:41 +0000

    partie html css affichage

---
+++ resources/views/Articles/liste.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud sur laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container text-center">
+   
+
+
+            <h1>Hello, world!</h1> <hr>
+            <a href="/ajouter" class="btn btn-primary">Ajouter un article</a><hr>
+            
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('image/businessman-student-working-from-home-while-being-isolated-keep-quarantine-coronavirus-covid-19.jpg') }}" class="card-img-top" alt="article one">
+
+                <div class="card-body">
+                  <h5 class="card-title">accessoire informatique</h5>
+                  <p class="card-text">Découvrez notre nouvelle gamme d'accessoires informatiques : claviers ergonomiques, souris précises, casques immersifs, et stations d'accueil. Améliorez confort et productivité avec élégance.</p>
+                  <a href="#" class="btn btn-primary">Voir plus</a>
+                </div>
+              </div>
+
+
+
+
+            
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
